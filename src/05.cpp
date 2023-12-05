b7d6333d3294ed3f4ab7f5eaf4e00b0930a62c27
Andrew Hardwick <drew.hardwick@gmail.com>
2023-12-05 06:41:23 -0600

    day 5 (mostly) done, some error in part 2

---
+++ src/05.cpp
@@ -0,0 +1,247 @@
+#include "daybase.hpp"
+
+#include "stringFunctions.h"
+
+#include <algorithm>
+#include <cstddef>
+#include <limits>
+#include <stdexcept>
+#include <string>
+#include <utility>
+
+
+typedef std::pair<std::vector<std::size_t>, std::vector<std::vector<std::vector<std::size_t>>>> primary;
+
+primary parse(
+		const std::vector<std::string>& file)
+{
+	auto seedSource = aocUtility::string::splitByDelimiter(file.at(0), ": ");
+	seedSource = aocUtility::string::splitByDelimiter(seedSource[1], " ");
+
+	std::vector<std::size_t> seeds;
+	for (auto e : seedSource)
+	{
+		seeds.push_back(std::stoul(e));
+	}
+
+	std::vector<std::string> map_source(file.begin() + 2, file.end());
+
+	std::vector<std::vector<std::size_t>> workingMap;
+	std::vector<std::vector<std::vector<std::size_t>>> maps;
+
+	for (auto line : map_source)
+	{
+		if (line.find("map") != std::string::npos)
+		{
+			workingMap = std::vector<std::vector<std::size_t>>();
+		}
+		else if (line.size() == 0)
+		{
+			maps.push_back(workingMap);
+		}
+		else
+		{
+			std::vector<std::size_t> mapEntry;
+			auto mapEntrySource = aocUtility::string::splitByDelimiter(line, " ");
+
+			for (auto e : mapEntrySource)
+			{
+				mapEntry.push_back(std::stoul(e));
+			}
+
+			workingMap.push_back(mapEntry);
+		}
+	}
+
+	maps.push_back(workingMap);
+
+	return std::make_pair(seeds, maps);
+}
+
+std::vector<std::pair<std::size_t, std::size_t>> applyMap(
+		const std::vector<std::pair<std::size_t, std::size_t>>& source,
+		const std::vector<std::vector<std::size_t>>& fullMap)
+{
+	std::vector<std::pair<std::size_t, std::size_t>> result;
+
+	std::vector<bool> handled;
+
+	for (std::size_t i = 0; i < source.size(); i++)
+	{
+		handled.push_back(false);
+	}
+
+	for (const auto& mapToApply : fullMap)
+	{
+		auto mapDest = mapToApply.at(0);
+		auto mapSour = mapToApply.at(1);
+		auto mapLeng = mapToApply.at(2);
+		auto mapEnd = mapSour + mapLeng - 1;
+
+		for (std::size_t i = 0; i < source.size(); i++)
+		{
+			if (handled.at(i))
+			{
+				continue;
+			}
+
+			const auto& pair = source.at(i);
+			const auto& [ start, length ] = pair;
+			auto last = start + length - 1;
+
+			bool handledThisCycle = false;
+
+			if (start >= mapSour && last <= mapEnd)
+			{
+				// encapsulated
+				result.push_back(std::make_pair(start + mapDest - mapSour, length));
+				handledThisCycle = true;
+			}
+			else if (start < mapSour && last > mapEnd)
+			{
+				// overlapping both sides
+
+				result.push_back(std::make_pair(start, mapSour - start));
+				result.push_back(std::make_pair(mapDest, mapLeng));
+				result.push_back(std::make_pair(mapEnd + 1, last - mapEnd));
+
+				handledThisCycle = true;
+			}
+			else if (start <= mapSour && last <= mapEnd && last >= mapSour)
+			{
+				// overlap to the left
+				auto length1 = mapSour - start;
+
+				result.push_back(std::make_pair(start, length1));
+				result.push_back(std::make_pair(mapDest, length - length1));
+				handledThisCycle = true;
+			}
+			else if (start >= mapSour && last >= mapEnd && start <= mapEnd)
+			{
+				// overlap to the right
+				auto length1 = mapEnd + 1 - start;
+
+				result.push_back(std::make_pair(start + mapDest - mapSour, length1));
+				result.push_back(std::make_pair(mapEnd + 1, length - length1));
+				handledThisCycle = true;
+			}
+
+			if (handledThisCycle)
+			{
+				handled.at(i) = true;
+			}
+
+			if (mapSour == 262282387 && handledThisCycle)
+			{
+				std::cout << "map info: " << mapDest << " " << mapSour << " " << mapEnd << "\n";
+				std::cout << "seed info: " << start << " " << last << "\n";
+
+				if (start < mapSour && last > mapEnd)
+				{
+					std::cout << "reverse encapsulated\n";
+				}
+
+			}
+		}
+	}
+
+	for (std::size_t i = 0; i < source.size(); i++)
+	{
+		if (!handled.at(i))
+		{
+			result.push_back(source.at(i));
+		}
+	}
+
+
+	return result;
+}
+
+std::size_t getMinSeed(
+		std::vector<std::pair<std::size_t, std::size_t>> seeds)
+{
+	std::size_t minSeed = std::numeric_limits<std::size_t>::max();
+	
+	for (auto [ start, length ] : seeds)
+	{
+		minSeed = std::min(start, minSeed);
+	}
+
+	return minSeed;
+}
+
+void testApplyMap()
+{
+	std::vector<std::pair<std::size_t, std::size_t>> seeds;
+
+	seeds.push_back(std::make_pair(10, 10));
+
+	std::vector<std::vector<std::size_t>> allMaps;
+
+	std::vector<std::size_t> testMap1 { 30, 10, 9};
+
+	allMaps.push_back(testMap1);
+
+	auto result = applyMap(seeds, allMaps);
+
+	for (auto [ s, l ] : result)
+	{
+		std::cout << s << " " << l << "\n";
+	}
+
+	throw std::domain_error("stop");
+}
+
+std::string partOne(
+		primary source)
+{
+	auto [ seedSource, maps ] = source;
+
+	std::vector<std::pair<std::size_t, std::size_t>> seeds;
+
+	for (auto seedLoc : seedSource)
+	{
+		seeds.push_back(std::make_pair(seedLoc, 1));
+	}
+
+	for (auto fullMap : maps)
+	{
+		seeds = applyMap(seeds, fullMap);
+	}
+
+	return std::to_string(getMinSeed(seeds));
+}
+
+std::string partTwo(
+		primary source)
+{
+	auto [ seedSource, maps ] = source;
+
+	std::vector<std::pair<std::size_t, std::size_t>> seeds;
+
+	for (std::size_t i = 0; i < seedSource.size(); i += 2)
+	{
+		std::cout << seedSource.at(i) << "   " << seedSource.at(i + 1) << "\n";
+		seeds.push_back(std::make_pair(seedSource.at(i), seedSource.at(i + 1)));
+	}
+
+	for (auto fullMap : maps)
+	{
+		seeds = applyMap(seeds, fullMap);
+	}
+
+	return std::to_string(getMinSeed(seeds));
+}
+
+int main(
+		int argc,
+		char** argv)
+{
+	auto filename = "input/2023_05_input";
+
+	auto parser = aocUtility::createWholesaleParserForFile<primary>(parse);
+
+	aocUtility::executeWholesale<primary>(filename, parser, partOne, partTwo);
+
+	return 0;
+}
--- utility/inc/daybase.hpp
+++ utility/inc/daybase.hpp
@@ -31,7 +31,50 @@ namespace aocUtility
 		std::string raw = inss.str();
 
 		// parse
-		std::vector<T> parsed = parser(raw);
+		auto parsed = parser(raw);
+
+		auto timeA = std::chrono::steady_clock::now();
+
+		std::string partOneResult = partOne(parsed);
+
+		auto timeB = std::chrono::steady_clock::now();
+
+		std::string partTwoResult = partTwo(parsed);
+
+		auto timeC = std::chrono::steady_clock::now();
+
+		auto durationPartOne = std::chrono::duration_cast<std::chrono::microseconds>(timeB - timeA).count();
+		auto durationPartTwo = std::chrono::duration_cast<std::chrono::microseconds>(timeC - timeB).count();
+
+		// execute (and print) part one
+		std::cout << "Part 1: " << partOneResult << "\n  - " << durationPartOne << "us\n";
+
+		// execute (and print) part two
+		std::cout << "Part 2: " << partTwoResult << "\n  - " << durationPartTwo << "us\n";
+	}
+
+	template<class T>
+	void executeWholesale(
+			std::string filename,
+			std::function<T(std::string)> parser,
+			std::function<std::string(T)> partOne,
+			std::function<std::string(T)> partTwo)
+	{
+		// load data from file
+		std::ifstream inf(filename);
+
+		if (inf.fail())
+		{
+			std::cout << "Unable to open input file: " << filename << "\n";
+			return;
+		}
+
+		std::stringstream inss;
+		inss << inf.rdbuf();
+		std::string raw = inss.str();
+
+		// parse
+		auto parsed = parser(raw);
 
 		auto timeA = std::chrono::steady_clock::now();
 
@@ -94,6 +137,27 @@ namespace aocUtility
 	template <class T>
 	std::function<std::vector<T>(std::string)> createParserForFile(
 			std::function<std::vector<T>(const std::vector<std::string>)> fileParser)
+	{
+		return [fileParser = std::move(fileParser)] (std::string source)
+		{
+			std::vector<std::string> rawSource;
+
+			std::stringstream inss(source);
+
+			std::string line;
+
+			while (std::getline(inss, line))
+			{
+				rawSource.push_back(line);
+			}
+
+			return fileParser(rawSource);
+		};
+	}
+
+	template <class T>
+	std::function<T(std::string)> createWholesaleParserForFile(
+			std::function<T(const std::vector<std::string>&)> fileParser)
 	{
 		return [fileParser = std::move(fileParser)] (std::string source)
 		{
